93c84b973070ea0ecd4efdd15fd280e6e1055522
fede183 <fede.183@hotmail.com>
2017-05-09 01:29:45 -0300

    Consulta 4(Sin testear)

--- consultas.sql
+++ consultas.sql
@@ -50,7 +50,7 @@ from Pais paisSuma)
 # 4. Dado un competidor(comp) la lista de categorias donde haya participado
 CREATE TEMP TABLE CompetenciasDeCompetidor AS select IdCompetencia as IdCompetencia from InscriptoEn IE where IE.DNIAlumno = comp.DNI Union select IdCompetencia from EquipoInscriptoEn EIE where EIE.IdEquipo = comp.IdEquipo
 
-CREATE TEMP TABLE CompetenciaCombateEquipoDeCompetidor AS select c.Tipo as Tipo, c.Sexo as Sexo, cce.Edad as Edad, NULL as Peso, ci.Graduacion as Graduacion from 
+CREATE TEMP TABLE CompetenciaCombateEquipoDeCompetidor AS select c.IdCompetencia as IdCompetencia, c.Tipo as Tipo, c.Sexo as Sexo, cce.Edad as Edad, NULL as Peso, ci.Graduacion as Graduacion from 
 Competencia c, CompetenciaCombateEquipo cce where c.IdCompetencia = cce.IdCompetencia and 
 Exists(select * from CompetenciasDeCompetidor cdc where cdc.IdCompetencia = c.IdCompetencia)
 
@@ -70,8 +70,32 @@ CREATE TEMP TABLE CompetenciaRoturaDeCompetidor AS select c.Tipo as Tipo, c.Sexo
 Competencia c, CompetenciaRotura cr , CompetenciaIndividual ci where ci.IdCompetencia = c.IdCompetencia and c.IdCompetencia = cr.IdCompetencia and 
 Exists(select * from CompetenciasDeCompetidor cdc where cdc.IdCompetencia = c.IdCompetencia)
 
+
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidorOroIndividual AS select cci.IdCompetencia as IdCompetencia, 'Oro' as Resultado from CompetenciaCombateIndividual cci where exists(select * from 
+cci.PrimerLugar = comp.DNI)
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidorOroIndividual AS select cci.IdCompetencia as IdCompetencia, 'Plata' as Resultado from CompetenciaCombateIndividual cci where exists(select * from 
+cci.SegundoLugar = comp.DNI)
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidorOroIndividual AS select cci.IdCompetencia as IdCompetencia, 'Bronce' as Resultado from CompetenciaCombateIndividual cci where exists(select * from 
+cci.TercerLugar = comp.DNI)
+
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidorOroEquipo AS select cce.IdCompetencia as IdCompetencia, 'Oro' as Resultado from CompetenciaCombateEquipo cce where exists(select * from 
+cce.PrimerLugar = comp.IdEquipo)
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidorPlataEquipo AS select cce.IdCompetencia as IdCompetencia, 'Plata' as Resultado from CompetenciaCombateEquipo cce where exists(select * from 
+cce.SegundoLugar = comp.IdEquipo)
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidorBronceEquipo AS select cce.IdCompetencia as IdCompetencia, 'Bronce' as Resultado from CompetenciaCombateEquipo cce where exists(select * from 
+cce.TercerLugar = comp.IdEquipo)
+
+CREATE TEMP TABLE ResultadosCompetenciasCompetidor AS ResultadosCompetenciasCompetidorOroEquipo union ResultadosCompetenciasCompetidorPlataEquipo union ResultadosCompetenciasCompetidorBronceEquipo union ResultadosCompetenciasCompetidorOroIndividual union ResultadosCompetenciasCompetidorPlataEquipo union ResultadosCompetenciasCompetidorBronceEquipo
+
 #El Resultado
-CompetenciaCombateEquipoDeCompetidor Union CompetenciaSaltoDeCompetidor Union CompetenciaFormasDeCompetidor Union CompetenciaCombateIndividualDeCompetidor Union CompetenciaRoturaDeCompetidor
+select c.Tipo as Tipo, c.Sexo as Sexo, c.Edad as Edad, c.Peso as Peso, c.Graduacion as Graduacion
+, c.Resultado as Resultado from ((CompetenciaCombateEquipoDeCompetidor Union CompetenciaSaltoDeCompetidor Union CompetenciaFormasDeCompetidor Union CompetenciaCombateIndividualDeCompetidor Union CompetenciaRoturaDeCompetidor) left join ResultadosCompetenciasCompetidor) c
 
 # 5. El medallero por escuela.
 CREATE TEMP TABLE OrosPorEscuela AS SELECT E.IdEscuela, E.Nombre, COUNT(*) AS Oro FROM Escuela E, Alumno A, InscriptoEn IE, CompetenciaIndividual CI WHERE E.IdEscuela = A.IdEscuela AND A.DNI = IE.DNI AND IE.IdCompetencia = CI.IdCompetencia AND CI.PrimerLugar = A.DNI GROUP BY E.IdEscuela;
